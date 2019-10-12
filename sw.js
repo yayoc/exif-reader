f63fe0dadea4bbc36904a622a326d752a52995f6
yayoc <n.yayoshi+github@gmail.com>
2019-10-12 21:39:29 +0900

    Introduce PWA

---
+++ sw.js
@@ -0,0 +1,17 @@
+const VERSION = "v1";
+
+self.addEventListener("install", function(e) {
+  e.waitUntil(
+    caches.open(`exif-reader-${VERSION}`).then(function(cache) {
+      return cache.addAll(["/", "/index.html", "/index.js"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
 const webpack = require("webpack");
 
 module.exports = {
@@ -12,6 +14,11 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "index.html",
     }),
+    new CopyWebpackPlugin([
+      { from: "./manifest.json", to: "manifest.json" },
+      { from: "./sw.js", to: "sw.js" },
+      { from: "./images", to: "images" },
+    ])
   ],
   mode: process.env.NODE_ENV,
 };
